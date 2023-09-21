b1b163de87e94dde5f235bec29b6a24063620e7d
Viktoria Rosovska <torimeister@gmail.com>
2023-09-21 16:37:26 +0300

    add mongodb connect

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const app = require("./app");
+const { mongoose, DB_HOST } = require("./mongodb/mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connecter success");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
